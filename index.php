85c2928b8763511d5c522df26738003469057bfc
Binyamin <7raivis@inbox.lv>
2016-10-12 23:36:44 +0300

    Menu order

--- index.php
+++ index.php
@@ -54,8 +54,8 @@ echo "\n<header>
     <div>
         <form method=post>
             <input list=feeds name=url type=url placeholder=\"Add feed address\" spellcheck=false aria-label=\"Enter feed address\" aria-autocomplete=list aria-controls=feeds required" . (strlen($url) || $openPage ? null : ' autofocus') . '><button aria-label=Achieve><svg viewBox="0 0 16 16" aria-hidden=true><path d="M11.7 10.3C12.4 9.6 13 8 13 6.5 13 3 10 0 6.5 0S0 3 0 6.5 3 13 6.5 13c1.6 0 3-.5 3.8-1.3l4.2 4.3 1.5-1.5-4.3-4.2zm-5.2.7C4 11 2 9 2 6.5S4 2 6.5 2 11 4 11 6.5 9 11 6.5 11z"/></svg></button>
-            <button id=about name=page value=about formnovalidate hidden>About</button>
             <button id=home name=url value="" formnovalidate hidden>Home</button>
+            <button id=about name=page value=about formnovalidate hidden>About</button>
             <datalist id=feeds>';
 
 foreach ($suggestions as $item) echo "\n                <option value=\"$item\">";
@@ -63,8 +63,8 @@ foreach ($suggestions as $item) echo "\n                <option value=\"$item\">
 echo "\n            </datalist>
         </form>
         <div class=flow>
-            <label for=about tabindex=0>About</label>
             <label for=home tabindex=0>Home</label>
+            <label for=about tabindex=0>About</label>
         </div>
     </div>
 </header>
